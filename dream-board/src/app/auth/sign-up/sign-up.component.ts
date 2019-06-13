c5b39daeda1d6369f98ff140a6fb612becea9aae
Faiza Latif <faizaj@agap2.pt>
2019-06-13 21:15:31 +0100

    page style

--- dream-board/src/app/auth/sign-up/sign-up.component.ts
+++ dream-board/src/app/auth/sign-up/sign-up.component.ts
@@ -8,19 +8,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
+  mandatoryFilled = true;
   constructor() {
-    this.signUpForm = this.SignUpFormGroup();
+    this.signUpForm = this.signUpFormGroup();
   }
 
   ngOnInit() {
+    this.signUpForm.statusChanges.subscribe(status => {
+      if (status === 'VALID') {
+        this.mandatoryFilled = false;
+      }
+    });
   }
 
-  SignUpFormGroup() {
+  signUpFormGroup() {
     return new FormGroup({
-      firstName: new FormControl([''], [Validators.required]),
-      lastName: new FormControl([''], [Validators.required]),
-      email: new FormControl([''], [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
       picture: new FormControl(''),
     });
   }
+
+  reset() {
+    this.signUpForm.reset();
+  }
+
+
 }
